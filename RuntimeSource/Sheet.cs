14f057b0b32d2c6b1f03a4a2695f5fb0e5038b57
giwon <geniikw@gmail.com>
2018-06-23 16:55:40 +0900

    Update Sheet.cs

--- RuntimeSource/Sheet.cs
+++ RuntimeSource/Sheet.cs
@@ -55,38 +55,4 @@ namespace geniikw.DataSheetLab
             return GetEnumerator();
         }
     }
-
-    public class DicSheet<TKey, T> : Sheet<T>
-    {
-        public List<TKey> keys = new List<TKey>();
-        private Dictionary<TKey, T> dic;
-        
-        private void OnEnable()
-        {
-            dic = new Dictionary<TKey, T>();
-            
-            if (keys.Count() != data.Count())
-            {
-                throw new Exception("must same. keys.Count : " + keys.Count() + " data.Count : " + data.Count());
-            }
-            for (int i = 0; i < keys.Count(); i++)
-            {
-                dic.Add(keys[i], data[i]);
-            }
-        }
-
-        public T this[TKey key]
-        {
-            get
-            {
-                return dic[key];
-            }
-        }
-        
-        public bool ContainsKey(TKey key)
-        {
-            return dic.ContainsKey(key);
-        }
-    }
-
 }
