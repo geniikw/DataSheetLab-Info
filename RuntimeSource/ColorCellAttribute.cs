f97f76d7e2f65cda1561aa82544d3824fb5d84bf
Kiwon Kim <geniikw@gmail.com>
2018-06-23 16:54:25 +0900

    add runtime sheet source

---
+++ RuntimeSource/ColorCellAttribute.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace geniikw.DataSheetLab
+{
+    public class BigCheck : Attribute
+    {
+        public float max;
+        public float r;
+        public float g;
+        public float b;
+
+        public BigCheck(float max, float r=1f, float g=0f, float b=0f)
+        {
+            this.max = max;
+            this.r = r; this.g = g; this.b = b;
+        }
+    }
+
+    public class SmallCheck : Attribute
+    {
+        public float min;
+        public float r;
+        public float g;
+        public float b;
+
+        public SmallCheck(float min, float r = 0f, float g = 0f, float b = 1f)
+        {
+            this.min = min;
+            this.r = r; this.g = g; this.b = b;
+        }
+    }
+    
+}
---
+++ RuntimeSource/Sheet.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+namespace geniikw.DataSheetLab
+{
+    public abstract class SheetBase : ScriptableObject{ }
+    
+    public class ReferSheet<TSheet, TItem> : IEnumerable<TItem>  where TSheet : Sheet<TItem>
+    {
+        public TSheet Sheet;
+        public List<int> idxList;
+
+        public TItem this[int idx] {
+            get
+            {
+                return Sheet[idxList[idx]];
+            }
+        }
+        
+        public IEnumerator<TItem> GetEnumerator()
+        {
+            foreach (var idx in idxList)
+                yield return Sheet[idx];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class Sheet<T> : SheetBase , IEnumerable<T>
+    {
+        public List<T> data = new List<T>();
+
+        public T this[int index]
+        {
+            get
+            {
+                return data[index];
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in data)
+                yield return item;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class DicSheet<TKey, T> : Sheet<T>
+    {
+        public List<TKey> keys = new List<TKey>();
+        private Dictionary<TKey, T> dic;
+        
+        private void OnEnable()
+        {
+            dic = new Dictionary<TKey, T>();
+            
+            if (keys.Count() != data.Count())
+            {
+                throw new Exception("must same. keys.Count : " + keys.Count() + " data.Count : " + data.Count());
+            }
+            for (int i = 0; i < keys.Count(); i++)
+            {
+                dic.Add(keys[i], data[i]);
+            }
+        }
+
+        public T this[TKey key]
+        {
+            get
+            {
+                return dic[key];
+            }
+        }
+        
+        public bool ContainsKey(TKey key)
+        {
+            return dic.ContainsKey(key);
+        }
+    }
+
+}
